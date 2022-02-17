7f3840144aa31c71b8fa4e2cfe037722378ea408
AngeloDalo <angelo251158@gmail.com>
2022-02-17 13:58:03 +0100

    PageController extends Controller

---
+++ laravel/resources/views/guest/movies.blade.php
@@ -0,0 +1,13 @@
+@extends('guest.layout.base')
+
+@section('documentTitle')
+    Movies
+@endsection
+
+@section('content')
+    Content
+@endsection
+
+@section('scripts')
+  <script src="{{asset('js/app.js')}}"></script>
+@endsection
